6ad164f326b83d8872a0b32aefb48425201f6b09
Dhhruvvv <118907757+Bhavik-punmiya@users.noreply.github.com>
2023-07-12 08:32:27 +0530

    hardhat-simplestorage

---
+++ hardhat.config.js
@@ -0,0 +1,47 @@
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("./tasks/block-number")
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
+require("solidity-coverage")
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const SEPOLIA_RPC_URL =
+  process.env.SEPOLIA_RPC_URL ||
+  "https://eth-sepolia.g.alchemy.com/v2/w9jeDpdoXxjhR15E0oSp4QG0P89mBly8"
+const PRIVATE_KEY =
+  process.env.PRIVATE_KEY ||
+  "16e574bfb9938063afe6de390e182db6a3d7041c66fdec4803555d7c26f1bb25"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "4BAZ8PA2BB1BF1JWBGXTU86QDZMJXJQZJ2"
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 31337,
+    },
+  },
+  solidity: "0.8.8",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+}
